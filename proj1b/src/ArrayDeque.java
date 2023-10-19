c34ee3bd03b7d7bb8ff38b3e7a21370dbbeea00a
kevincheng <kevincheng401@gmail.com>
2023-10-19 18:02:49 +0800

    Finish resize

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+    /** invariants:
+    addLast: The next Item we want to add, will go into position size.
+    getLast: The item we want to return is in position size - 1.
+    size: The number of items in the list should be size.
+    The first item in ArrayList is in position nextFirst + 1
+    get: The index we want is in position nextFirst + 1 + index
+    The number of items from nextFirst to the end of the items[] is (items.length - 1) - nextFirst
+    nextFirst is in position: capacity - distance - 1
+
+    */
+    private int size, nextFirst, nextLast;
+    private T[] items;
+
+    /** Helper function to return actual index. */
+    private int targetIndexInItems(int index){
+        if (index >= items.length){
+            return index - items.length;
+        } else if (index < 0) {
+            return items.length - 1;
+        }
+        return index;
+    }
+
+    /** Helper function for checking if the ArrayDeque needs to resize or not. */
+    private boolean isNeedResize(int s){
+        if (s == items.length){
+            return true;
+        }
+        return false;
+    }
+
+    /** Helper function for calculating the position of ArrayDeque[0] in new items */
+    private int firstItemStartingPosition(int capacity, int distance){
+//        System.out.println(capacity - distance);
+        return capacity - distance;
+    }
+
+    private void resize(int capacity){
+        T[] a = (T[]) new Object[capacity]; // capacity: a.length == 16;
+
+        // nextFirst to end
+        int distance = items.length - 1 - nextFirst;
+        int firstPos = firstItemStartingPosition(capacity, distance);
+        for (int i = 0; i < distance; i += 1){
+            a[firstPos + i] = items[nextFirst + 1 + i];
+        }
+        nextFirst = capacity - distance - 1;
+
+        // head to nextLast
+        distance = nextLast;
+        for (int i = 0; i < distance; i += 1){
+            a[i] = items[i];
+        }
+
+        items = a;
+//        for (int i = 0; i < capacity; i += 1){
+//            System.out.print(items[i]+" ");
+//        }
+    }
+
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+
+    @Override
+    public void addFirst(T x) {
+        nextFirst = this.targetIndexInItems(nextFirst);
+        if (isNeedResize(size)){
+            resize(size * 2);
+        }
+        items[nextFirst] = x;
+        size += 1;
+        nextFirst -= 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        nextLast = this.targetIndexInItems(nextLast);
+        if (isNeedResize(size)){
+            resize(size * 2);
+        }
+        items[nextLast] = x;
+        size += 1;
+        nextLast += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 1; i <= size; i += 1){
+            returnList.add(items[this.targetIndexInItems(nextFirst + i)]);
+        }
+        return returnList;
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index > size - 1 || index < 0){
+            return null;
+        }
+        return items[targetIndexInItems(nextFirst + 1 + index)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Deque;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -10,14 +11,97 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
-    // @Test
-    // @DisplayName("ArrayDeque has no fields besides backing array and primitives")
-    // void noNonTrivialFields() {
-    //     List<Field> badFields = Reflection.getFields(ArrayDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-    //             .toList();
+     @Test
+     @DisplayName("ArrayDeque has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
-    //     assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-    // }
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+     @Test
+     public void addFirstTest(){
+         ArrayDeque<Integer> a = new ArrayDeque<>();
+         a.addFirst(1);
+         a.addFirst(2);
+         a.addFirst(3);
+         assertThat(a.toList()).containsExactly(3, 2, 1).inOrder();
+     }
+
+     @Test
+     public void addLastTest(){
+         ArrayDeque<Integer> a = new ArrayDeque<>();
+         a.addLast(1);
+         a.addLast(2);
+         a.addLast(3);
+         assertThat(a.toList()).containsExactly(1,2,3).inOrder();
+     }
+
+     @Test
+     public void resizeTest(){
+         ArrayDeque<Integer> a = new ArrayDeque<>();
+         a.addFirst(1);
+         a.addLast(2);
+         a.addLast(4);
+         a.addLast(6);
+         a.addLast(7);
+         a.addFirst(3);
+         a.addFirst(5);
+         a.addFirst(8);
+         a.addFirst(100);
+         a.addFirst(234);
+         a.addLast(567);
+         assertThat(a.toList()).containsExactly(234,100,8,5,3,1,2,4,6,7,567).inOrder();
+     }
+
+     @Test
+    public void getTest(){
+         ArrayDeque<Integer> a = new ArrayDeque<>();
+         a.addFirst(1);
+         a.addLast(2);
+         a.addFirst(3);
+         a.addLast(4);
+         a.addFirst(5);
+         a.addLast(6); // ArrayDeque: [nextFirst, 5, 3, 1, 2, 4, 6, nextLast]
+                          //      items: [1, 2, 4, 6, nextLast, nextFirst, 5, 3]
+         assertThat(a.get(0)).isEqualTo(5);
+         assertThat(a.get(5)).isEqualTo(6);
+//         assertThat(a.get(6)).isNull();
+     }
+
+     @Test
+     public void sizeTest(){
+         ArrayDeque<String> a = new ArrayDeque<>();
+         a.addFirst("abcs");
+         a.addLast("qwiduh");
+         a.addLast("21easd");
+         assertThat(a.size()).isEqualTo(3);
+     }
+
+     @Test
+    public void isEmptyTest(){
+         ArrayDeque<String> a = new ArrayDeque<>();
+         assertThat(a.isEmpty()).isTrue();
+         a.addLast("qwe");
+         a.addFirst("12312");
+         assertThat(a.isEmpty()).isFalse();
+     }
+
+     @Test
+    public void removeFirstTest(){
+         ArrayDeque<Integer> a = new ArrayDeque<>();
+         a.addFirst(1);
+         a.addLast(2);
+         a.addFirst(3);
+         a.addLast(4);
+         a.addFirst(5);
+         a.addLast(6); // [5, 3, 1, 2, 4, 6]
+         a.removeFirst(); // [3, 1, 2, 4, 6]
+         assertThat(a.toList()).containsExactly(3, 1, 2, 4, 6).inOrder();
+
+     }
+
 
 }
