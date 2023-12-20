169ea10b6be8991de2490c9fe7c8475930a6842f
kevincheng <kevincheng401@gmail.com>
2023-12-20 17:57:37 +0800

    restart hw2

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -2,51 +2,38 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-    Site[][] sites;
-    WeightedQuickUnionUF uf;
-    int openNum;
-    public class Site{
-        int row;
-        int col;
-        int oneD;
+    // TODO: Add any necessary instance variables.
+
+    public class Site {
+        int row, col, xy;
         boolean isOpen;
-        boolean isFull;
-        public Site(int r, int c){
-            this.row = r;
-            this.col = c;
-            this.oneD = xyTo1D(r,c);
-            this.isOpen = false;
-            this.isFull = false;
+        public Site(int x, int y){
+            row = x;
+            col = y;
+            xy = xyTo1d(x, y);
         }
     }
-
     public Percolation(int N) {
-        sites = new Site[N][N];
-        for (int r = 0; r < N; r += 1){
-            for (int c = 0; c < N; c += 1){
-                sites[r][c] = new Site(r, c);
-            }
-        }
-        openNum = 0;
-        uf = new WeightedQuickUnionUF(N * N);
+        // TODO: Fill in this constructor.
     }
 
     public void open(int row, int col) {
-        sites[row][col].isOpen = true;
-        openNum += 1;
-        connect(xyTo1D(row,col));
+        // TODO: Fill in this method.
     }
 
     public boolean isOpen(int row, int col) {
-        return sites[row][col].isOpen;
+        // TODO: Fill in this method.
+        return false;
     }
 
     public boolean isFull(int row, int col) {
-        return sites[row][col].isFull;
+        // TODO: Fill in this method.
+        return false;
     }
 
     public int numberOfOpenSites() {
-        return openNum;
+        // TODO: Fill in this method.
+        return 0;
     }
 
     public boolean percolates() {
@@ -55,43 +42,11 @@ public class Percolation {
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
-    public int xyTo1D(int r, int c){
-        return r * 5 + c;
-    }
+    // TODO: Remove all TODO comments before submitting.
 
-    /** helper function to check if is connected */
-    public void connect(int i){
-        int t = i - 5;
-        int b = i + 5;
-        int l = i - 1;
-        int r = i + 1;
-        if (t >= 0){
-            if(sites[t / 5][t % 5].isOpen){
-                uf.union(i, t);
-//                System.out.println("union top");
-            }
-        }
-        if (l >= 0 && (l % 5) != 4){
-            if(sites[l / 5][l % 5].isOpen){
-                uf.union(i, l);
-//                System.out.println("union left");
-
-            }
-        }
-        if ((r % 5) != 0){
-            if(sites[r / 5][r % 5].isOpen){
-                uf.union(i, r);
-//                System.out.println("union right");
-
-            }
-        }
-        if (b < 25){
-            if(sites[b / 5][b % 5].isOpen){
-                uf.union(i, b);
-//                System.out.println("union bottom");
-
-            }
-        }
+    /* Helper function to convert x, y to 1-d */
+    public int xyTo1d(int x, int y){
+        return x * 5 + y;
     }
 
 }
--- hw2/tests/PercolationTest.java
+++ hw2/tests/PercolationTest.java
@@ -43,61 +43,6 @@ public class PercolationTest {
     @Test
     public void yourTestHere() {
         // TODO: write some more tests
-        int N = 5;
-        Percolation p = new Percolation(5);
-        System.out.println(p);
-    }
-
-    @Test
-    public void isOpenTest() {
-        int N = 5;
-        Percolation p = new Percolation(5);
-        p.open(0,0);
-        p.open(4,4);
-        assertThat(p.isOpen(0,0)).isTrue();
-        assertThat(p.isOpen(4,4)).isTrue();
-    }
-
-    @Test
-    public void numberOfOpenSitesTest() {
-        int N = 5;
-        Percolation p = new Percolation(5);
-        p.open(0,0);
-        p.open(4,4);
-        assertThat(p.numberOfOpenSites()).isEqualTo(2);
-
-    }
-
-    @Test
-    public void disjointSetTest() {
-        int N = 5;
-        Percolation p = new Percolation(5);
-        System.out.println(p.uf.find(24));
-
-    }
-
-    @Test
-    public void xyTo1DTest(){
-        int N = 5;
-        Percolation p = new Percolation(5);
-        assertThat(p.sites[3][2].oneD).isEqualTo(17);
-    }
-
-    @Test
-    public void connectTest(){
-        int N = 5;
-        Percolation p = new Percolation(5);
-        p.open(0,0);
-        p.open(1, 0);
-        assertThat(p.uf.connected(0,5)).isTrue();
-    }
-
-    @Test
-    public void percolateTest(){
-        int N = 5;
-        Percolation p = new Percolation(5);
-        p.open(0,0);
-        p.open(1, 0);
     }
 
 }
